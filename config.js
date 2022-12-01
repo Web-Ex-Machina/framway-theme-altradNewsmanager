b58eb8cfdadd24a64c944d07839a26cc25f60ef9
Quentin Vansteene <vansteene.quentin@gmail.com>
2022-12-01 19:18:42 +0100

    set config the btn exclude config to false

--- config.js
+++ config.js
@@ -1,1 +1,10 @@
-module.exports = {};
+module.exports = {
+	'btn':{
+		'exclude': false,
+	},
+	'input-valid': {
+		'background': 'input(background)',
+		'font-color': 'input(font-color)',
+		'border-color': 'success',
+	},
+};
